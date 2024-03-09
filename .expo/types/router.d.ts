b8346a7984dd73b0a157f695dc6f2c0ecb408cea
jhessica <jhessfsantos@gmail.com>
2024-03-08 23:02:34 -0300

    adicionando projeto

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -7,11 +7,11 @@ declare module "expo-router" {
   export * from 'expo-router/build';
 
   // prettier-ignore
-  type StaticRoutes = `/` | `/index/` | `/index/styles` | `/recipes/styles` | `/recipe/styles`;
+  type StaticRoutes = `/` | `/index/` | `/index/styles` | `/recipe/styles` | `/recipes/styles`;
   // prettier-ignore
-  type DynamicRoutes<T extends string> = `/recipes/${SingleRoutePart<T>}` | `/recipe/${SingleRoutePart<T>}`;
+  type DynamicRoutes<T extends string> = `/recipe/${SingleRoutePart<T>}` | `/recipes/${SingleRoutePart<T>}`;
   // prettier-ignore
-  type DynamicRouteTemplate = `/recipes/[ingredientsIds]` | `/recipe/[id]`;
+  type DynamicRouteTemplate = `/recipe/[id]` | `/recipes/[ingredientsIds]`;
 
   type RelativePathString = `./${string}` | `../${string}` | '..';
   type AbsoluteRoute = DynamicRouteTemplate | StaticRoutes;
